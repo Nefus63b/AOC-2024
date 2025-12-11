d765a7eacf673b0c74fcf05a0ffda0229e010e80
Xavier MANSARD <xavier.mansard@gmail.com>
2025-12-11 21:17:44 +0100

    Day 6 part 2 début

--- AOC2025/Days/Day6.cs
+++ AOC2025/Days/Day6.cs
@@ -69,7 +69,46 @@ namespace AOC2025.Days
         {
             SantaFileReader data = new SantaFileReader(Path.Combine("Inputs", $"Day{Day}.txt"), ' ');
 
-            Res_Part2 = 0;
+            var operations = new (char Operation, List<long> Values)[data.ColumnsCount];
+            var lines = data.GetAllLines().ToList();
+            var lastLine = lines.Last();
+            // Le signe donne le nb de char
+
+            for (int i = 0; i < data.ColumnsCount; i++)
+            {
+                operations[i] = (Operation: lastLine[i].Trim()[0], Values: new List<long>());
+            }
+
+            for (int colIndex = 0; colIndex < data.ColumnsCount; colIndex++)
+            {
+                for (int lineIndex = 0; lineIndex < lines.Count - 1; lineIndex++)
+                {
+                    var numberStr = lines[lineIndex][colIndex].Replace(" ", "0").Trim();
+                }
+            }
+
+            long total = 0;
+            foreach (var operation in operations)
+            {
+                switch (operation.Operation)
+                {
+                    case '+':
+                        total += operation.Values.Sum();
+                        break;
+                    case '*':
+                        long prod = 1;
+                        foreach (var val in operation.Values)
+                        {
+                            prod *= val;
+                        }
+                        total += prod;
+                        break;
+                    default:
+                        throw new InvalidOperationException($"Operation {operation.Operation} is not supported.");
+                }
+            }
+
+            Res_Part2 = total;
         }
     }
                     
